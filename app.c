ec901f91040eb4acc4d5cf804f6c61e3c51d48ef
Gabriel-Bjay <jayabjay2018@gmail.com>
2024-12-30 20:00:41 +0300

    Data type example

--- app.c
+++ app.c
@@ -1,29 +1,14 @@
 #include <stdio.h>
-
 int main() {
-    printf("Hello, World!\n");
-    // Declare and initialize variables
-    int myNum = 5; // Integer (whole number)
-    int myOtherNum = 25;   // Integer (whole number)
-    int x;
-    float myFloatNum = 5.5; // Floating point number
-    char myString = 'D';   // Character
-    // Assign value of myNum to x
-    x = myNum;
-    //Adding variables together
-    int sum = myNum + myOtherNum;
-    int sum2 = sum + x;
+    // Create variables of different data types
+    int items = 27;
+    float cost_per_item = 12.75;
+    char currency = '$';
 
-    //Student Data
-    int studentId = 1738;
-    float studentGrade = 73.81;
-    // Print the values of the variables
-    printf("%d\n", myNum);
-    printf("%d\n", sum);
-    printf("%d\n", sum2);
-    printf("%d\n", studentId);
-    printf("%f\n", myFloatNum);
-    printf("%f\n", studentGrade);
-    printf("%c\n", myString);
+    float total_cost = items * cost_per_item;
+    // Print variables
+    printf("Number of items: %d\n", items);
+    printf("Cost per Item: %.2f %c\n", cost_per_item, currency);
+    printf("Total cost = %.2f %c\n", total_cost, currency);
     return 0;
 }
