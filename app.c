5b0c0933e1cecea75d31ce25b880585f56ed3a1c
Gabriel-Bjay <jayabjay2018@gmail.com>
2024-12-30 21:25:49 +0300

    If-Else statements

--- app.c
+++ app.c
@@ -1,14 +1,36 @@
 #include <stdio.h>
 int main() {
-    // Create variables of different data types
-    int items = 27;
-    float cost_per_item = 12.75;
-    char currency = '$';
-
-    float total_cost = items * cost_per_item;
-    // Print variables
-    printf("Number of items: %d\n", items);
-    printf("Cost per Item: %.2f %c\n", cost_per_item, currency);
-    printf("Total cost = %.2f %c\n", total_cost, currency);
+    //If else statements
+    int a = 111;
+    int b = 100;
+    if(a > b){
+        printf("%d is greater\n", a);
+    }else{
+        printf("%d is greater\n", b);
+    }
+    //If else if statements
+    int age = -17;
+    int votingAge = 18;
+    if(age >= votingAge){
+        printf("You are eligible to vote\n");
+    }else if(age < votingAge && age > 0){
+        printf("You are not eligible to vote\n");
+    }else{
+        printf("Invalid age\n");
+    }
+    //Nested if else statements
+    int num = 30;
+    if (num > 0) {
+        printf("%d is positive.\n", num);
+        if (num % 2 == 0) {
+            printf("%d is even.\n", num);
+        } else { printf("%d is odd.\n", num);
+        }
+    } else {
+        printf("%d is negative or zero.\n", num);
+    }
+    //shorthand if statements
+    int time = 20;
+    (time <= 18) ? printf("Good day.\n") : printf("Good evening.\n");
     return 0;
 }
