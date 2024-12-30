34218654258a8f9172f808faf158e7e57e5667e5
Gabriel-Bjay <jayabjay2018@gmail.com>
2024-12-30 13:01:16 +0300

    C basics

---
+++ app.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    int myNum = 22;
+    float myFloatNum = 22.22;
+    char myString = 'D';
+    printf("%d\n", myNum);
+    printf("%f\n", myFloatNum);
+    printf("%c\n", myString);
+    return 0;
+}
